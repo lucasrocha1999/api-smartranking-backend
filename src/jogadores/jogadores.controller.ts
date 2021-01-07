f87ae3b22cb37030818ccb05cbf3b2106896f1ad
Lucas Lira <lucasrocha446@gmail.com>
2021-01-06 21:00:39 -0300

    feat: adding custom pipe

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -11,6 +11,7 @@ import {
 import { JogadoresService } from './jogadores.service';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
+import { jogadoresValidacaoParametrosPipe } from './pipes/jogadores-validacao-parametros.pipe';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -24,7 +25,7 @@ export class JogadoresController {
 
   @Get()
   async consultarJogadores(
-    @Query('email') email: string,
+    @Query('email', jogadoresValidacaoParametrosPipe) email: string,
   ): Promise<Jogador | Jogador[]> {
     if (email) {
       return this.jogadoresService.consultarJogadorPeloEmail(email);
@@ -34,7 +35,9 @@ export class JogadoresController {
   }
 
   @Delete()
-  async deletarJogador(@Query('email') email: string): Promise<void> {
+  async deletarJogador(
+    @Query('email', jogadoresValidacaoParametrosPipe) email: string,
+  ): Promise<void> {
     this.jogadoresService.deletarJogador(email);
   }
 }
