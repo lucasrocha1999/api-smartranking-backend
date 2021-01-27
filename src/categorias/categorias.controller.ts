ffbc6f892cd1b5a89d412cca6c1d078c974d79ae
Lucas Lira <lucasrocha446@gmail.com>
2021-01-26 22:59:19 -0300

    feat: add module categorias

---
+++ src/categorias/categorias.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CategoriasService } from './categorias.service';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+import { Categoria } from './interface/categoria.interface';
+
+@Controller('api/v1/categorias')
+export class CategoriasController {
+  constructor(private readonly categoriasService: CategoriasService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async criarCategoria(
+    @Body() criarCategoriaDto: CriarCategoriaDto,
+  ): Promise<Categoria> {
+    return await this.categoriasService.criarCategoria(criarCategoriaDto);
+  }
+
+  @Get()
+  async consultarCategorias(): Promise<Array<Categoria>> {
+    return await this.categoriasService.consultarTodasCategorias();
+  }
+
+  @Get('/:categoria')
+  async consultarCategoriaPeloId(
+    @Param('categoria') categoria: string,
+  ): Promise<Categoria> {
+    return await this.categoriasService.consultarCategoriaPeloId(categoria);
+  }
+}
